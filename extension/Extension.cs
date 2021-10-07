b23c2d19e9f988be7e5b1c2366937e23dc6d788e
Suleymanha <suleymanha@code.edu.az>
2021-10-07 11:21:39 +0400

    Add project files.

---
+++ extension/Extension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace extension
+{
+    static class Extension
+    {
+        
+        public static bool  Contain(this string str,string letter)
+        {
+            int temp = 0;
+            for (int i=0; i<str.Length;i++)
+            {
+                if (str[i] == letter[0])
+                {
+                    for( int n=i+1,m=1; n < letter.Length; n++, m++)
+                    {
+                        if (str[n].ToString().ToLower() == letter[m].ToString().ToLower())
+                        {
+                            temp++;
+                        }
+                    }
+                }
+            }
+            if (temp == letter.Length - 1)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
---
+++ extension/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello world";
+
+            Console.WriteLine(str.Contains("hel"));
+        }
+
+
+    }
+    
+}
